822668cd4d3ac8bf7155c77de64b93aff711d659
Peter Lam <Piitaa1234@gmail.com>
2020-04-10 16:06:05 -0400

    Fixed Script.js for generator and fixed slider bar

--- 02-Homework/Develop/script.js
+++ 02-Homework/Develop/script.js
@@ -1,13 +1,35 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+var generateBtn = document.querySelector(".btn");
+var copyBtn = document.querySelector(".btn");
+var slider = document.getElementById("myRange");
+var output = document.getElementById("demo");
+output.innerHTML = slider.value;
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
+// Generating Password
+function genPass() {
+  // length of password
+  var passLen = slider.value;
+  // variable to hold password
+  var values =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!@#$%^&*()_+";
+  // for loop for password
+  let passwords = "";
+  for (var i = 1; i <= passLen; i++) {
+    passwords += values.charAt(
+      Math.floor(Math.random() * Math.floor(values.length - 1))
+    );
+  }
+  console.log(genPass);
+  // Add event listener to generate button
+  document.querySelector("#password").textContent = passwords;
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+// Copy Password to Clipboard
+function copyPass() {}
+
+slider.oninput = function () {
+  output.innerHTML = this.value;
+};
+
+generateBtn.addEventListener("click", genPass);
+copyBtn.addEventListener("click", copyPass);
